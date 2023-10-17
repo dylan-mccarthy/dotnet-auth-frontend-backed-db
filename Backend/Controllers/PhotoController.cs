9b00f0e8ce3b59662c8348cb33e6b80cbfaf6536
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-10-17 11:28:08 +0000

    Adding todo for future

--- Backend/Controllers/PhotoController.cs
+++ Backend/Controllers/PhotoController.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using AstroApp.Backend.Services;
+using AstroApp.Shared.Models;
 
 namespace AstroApp.Backend.Controllers
 {
     public class PhotoController : ControllerBase
     {
-
+        private IPhotoService _photoService;
+        public PhotoController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
     }
 }
---
+++ TODO.md
@@ -0,0 +1,18 @@
+# General
+* Create Architecture Diagram
+* Create README.md
+
+# Infrastructure 
+* Create basic infrastructure for Azure using Bicep
+* Create infrastructure Pipeline using GH Actions
+* Automate creation of DB and adding of DB User
+
+# Backend
+* Create Photo Upload Service -> Using a Storage Account
+* Determine best way to serve Photos (Assuming this is storage account)
+
+# Frontend
+* Create basic pages
+* Create Authentication
+
+
